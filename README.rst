ac68674b4d63a0a1da34393af21c33cb1ddcebb3
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-06-20 22:58:26 -0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+Description
+===========
+
+A Flip_ interpreter with nice QT interface writed by Roberto Alsina hosted orginaly on Google_.
+I will use this repository to organice my changes.
+
+Colaborate
+========
+
+You are free to clone this project from http://github.com/pointtonull/ra-flip.git
+or to report issues/whises on http://github.com/pointtonull/ra-flip/issues
+
+.. _Google: http://code.google.com/p/ra-flip
+.. _Flip: http://www.daimi.au.dk/~eriksoe/Flip/language.html
---
+++ mailsmasivos.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+from debug import debug
+from decoradores import Verbose, Async
+from email.Header import Header
+from email.MIMEText import MIMEText
+import random
+import smtplib
+import sys
+import time
+import socket
+from itertools import islice
+
+VERBOSE = 2
+
+"""
+Los archivos direcciones.txt y omitir.txt son listas de direcciones de mail
+separados por enter. Ambos deben existir.
+
+Los archivos accounts.txt y noaccounts.txt son listas de pares
+usuario;contraseña. Ambos deben existir.
+
+El fichero mensaje.txt es el mensaje que se enviará. La primera linea de este
+archivo será usada como titulo del mensaje.
+
+Se debe modificar la dirección de remitente en la linea 60.
+"""
+
+
+def write(text, destination):
+    f = open(destination, "a")
+    f.write("%s\n" % text)
+    f.close()
+
+
+@Async
+@Verbose(VERBOSE)
+def enviar(destinos, titulo, cuerpo, server=None):
+    coding = 'utf8'
+
+    if server is None:
+        while not server:
+            account = random.choice(get_accounts())
+            smtp, user, password = account.strip().split(";")
+            server = get_server(smtp, user, password)
+
+    for destino in destinos:
+        msg = MIMEText(cuerpo.encode(coding), 'plain', coding)
+        msg["From"] = user
+        msg["Subject"] = Header(titulo, coding)
+        msg["To"] = destino
+        debug("   %40s > %s" % (user, destino))
+        try:
+            server.sendmail(user, destino, msg.as_string())
+            write(destino, "omitir.txt")
+        except smtplib.SMTPDataError, e:
+            debug("Calmandome un poco... %s" % e)
+            raise
+        except socket.sslerror:
+            pass
+
+    server.close()
+
+
+def get_server(url, user, password):
+    server = smtplib.SMTP()
+    server.connect(url)
+    server.ehlo()
+    server.starttls()
+
+    try:
+        server.login(user, password)
+    except smtplib.SMTPAuthenticationError or socket.sslerror:
+        write("%s;%s;%s" % (url, user, password), "noaccounts.txt")
+        return
+    except socket.sslerror:
+        return
+
+    return server
+
+
+def get_accounts():
+    accounts = set(open("accounts.txt").readlines())
+    try:
+        noaccounts = set(open("noaccounts.txt").readlines())
+    except IOError:
+        debug("W: No existe el fichero noaccounts.txt")
+        noaccounts = set()
+    return list(accounts - noaccounts)
+
+
+def partition(iter, partsize):
+    return (islice(iter, npart * partsize, npart * partsize + partsize)
+        for npart in xrange(len(iter) / partsize + 1))
+
+
+if __name__ == "__main__":
+
+    mensaje = open("mensaje.txt").readlines()
+    titulo = unicode(mensaje[0].strip(), "UTF8")
+    cuerpo = unicode("".join(mensaje[1:]).strip(), "UTF8")
+
+    dirs = set((s.lower().strip()
+        for s in open("direcciones.txt").readlines()))
+    omitir = set((s.strip() for s in open("omitir.txt").readlines()))
+
+    dirs = sorted(dirs - omitir)
+
+    debug("Enviando a %d destinatarios.\n" % len(dirs))
+
+    partsize = 10
+    threads = 40
+    slots = [None] * threads
+    
+    for part in partition(dirs, partsize):
+        passed = False
+        while not passed:
+            for pos in xrange(len(slots)):
+                if slots[pos] is None or not slots[pos].is_alive():
+                    slots[pos] = enviar(part, titulo, cuerpo)
+                    passed = True
+                    break
+            time.sleep(1)
+
+    debug("EOF!!")
